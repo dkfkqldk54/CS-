dd185faa5b32e30ba0174b31923da8a1a38c5875
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-10-17 19:19:40 +0900

    Update AVLTree.py

--- Data Structure/쉽게 배우는 자료구조 with 파이썬/알고리즘 모음/AVLTree.py
+++ Data Structure/쉽게 배우는 자료구조 with 파이썬/알고리즘 모음/AVLTree.py
@@ -105,3 +105,65 @@ def __deleteMinItem(self, tNode):
     return (rtnItem, tNode)
   
 #균형 잡기
+def __balanceAVL(self, tNode, type):
+  returnNode = self.NIL
+  if type == self.LL:
+    returnNode = self.__rightRotate(tNode)
+  elif tyep == self.LR:
+    tNode.left = self.__leftRotate(tNode.left)
+    returnNode = self.__rightRotate(tNode)
+  elif type == self.RR:
+    returnNode = self.__leftRotate(tNode)
+  elif type == self.RL:
+    tNode.right = self.__rightRotate(tNode.right)
+    returnNode = self.__leftRotate(tNode)
+  else:
+    print("Impossible type: Should be one of LL, LR, RR, RL")
+  return returnNode
+
+#좌회전
+def __leftRotate(self, t):
+  Rchild = t.right
+  if Rchild == self.NIL:
+    print(t.item, "'s Rchild shouldn't be NIL!")
+  RLchild = Rchild.left
+  Rchild.left = t
+  t.right = RLchild
+  t.height = 1 + max(t.left.height, t.right,height)
+  Rchild.height = 1 + max(Rchild.left.height, Rchild.right.height)
+  return Rchild
+
+#우회전
+def __rightRotate(self, t):
+  Lchild = t.left
+  if Lchild == self.NIL:
+    print(t.item, "'s Lchild shouldn't be NIL!")
+  LRchild = Lchild.right
+  Lchild.right = t
+  t.left = LRchild
+  t.height = 1 + max(t.left.height, t.right,height)
+  Lchild.height = 1 + max(Lchild.left.height, Lchild.right.height)
+  return Lchild
+
+def __needBalance(self, t):
+  type = self.ILLEGAL
+  if t.left.height + 2 <= t.right.height:
+    if t.right.left.height <= t.right.right.height:
+      type = self.RR
+    else:
+      type = self.RL
+  elif t.left.height >= t.right.height+2:
+    if t.left.left.height >= t.left.right.height:
+      type = self.LL
+    else:
+      type = self.LR
+  else:
+    type = self.NO_NEED
+  return type
+
+#기타
+def isEmpty(self):
+  return self.__root == self.NIL
+
+def clear(self):
+  self.__root = self.NIL
