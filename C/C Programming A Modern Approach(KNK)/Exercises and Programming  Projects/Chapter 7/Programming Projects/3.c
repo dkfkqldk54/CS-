840e6dceb42f51c5277512f7f1bba84b9dd48f91
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-11-07 18:17:49 +0900

    Create 3.c

---
+++ C/C Programming A Modern Approach(KNK)/Exercises and Programming  Projects/Chapter 7/Programming Projects/3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    double n, sum = 0.0;
+
+    printf("This program sums a series of floats.\n");
+    printf("Enter float (0 to terminate): ");
+    scanf("%lf", &n);
+
+    while (n!=0.0) {
+      sum += n;
+      scanf("%lf", &n);
+    }
+
+    printf("The sum is: %lf\n", sum);
+    
+    return 0;
+}
