5b46851f7857dafdcf1074b0c199c06fc26b0a99
Seongjin Heo <63328796+dkfkqldk54@users.noreply.github.com>
2022-11-18 21:54:23 +0900

    Create 1.c

---
+++ C/C Programming A Modern Approach(KNK)/Exercises and Programming  Projects/Chapter 8/Programming Projects/1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int digit_seen[10] = {0}, digit, i;
+  long n;
+
+  printf("Enter a number: ");
+  scanf("%ld", &n);
+
+  while (n>0) {
+    digit = n % 10;
+    digit_seen[digit] += 1;
+    n /= 10;
+  }
+
+  printf("Repeated digit(s): ");
+
+  for (i=0; i<10; i++) {
+    if (digit_seen[i] >= 2) {
+      printf(" %d", i);
+      }
+  }
+  
+  return 0;
+}
